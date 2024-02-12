1d61afa812e0deca4550e3bf3ad0c53b106f4f70
Henrique <rodrigues.henrique.1984@gmail.com>
2024-02-12 14:27:28 +0100

    First commit

---
+++ src/Addition.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Math;
+
+class Addition
+{
+
+    public function deux(int $n1, int $n2):int
+    {
+        return $n1 + $n2;
+    }
+
+    public function trois(int $n1, int $n2, int $n3):int
+    {
+        return $n1 + $n2 + $n3;
+    }
+
+}
